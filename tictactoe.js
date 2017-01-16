15928162e62f520689678c1e9859fd9b8e6721b7
Chad Griffis <cmg123@me.com>
2017-01-16 15:28:24 -0600

    adds the playerMove and checkWin functionality

--- tictactoe.js
+++ tictactoe.js
@@ -12,4 +12,27 @@ let boardPrint =
 
 const winSequences = [[1, 2, 3], [4, 5, 6],[7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
+const playerMove = function(position, mark) {
+  board[position] = mark;
+}
+
+const checkWin = function(mark) {
+  let markCount = 0;
+  for(let i = 0; i < winSequences.length; i++) {
+    for(let j = 0; j < winSequences[i].length; j++) {
+      if(winSequences[i][j] === mark) {
+        markCount++;
+      }
+      if (markCount === 3) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+const play = function(player) {
+
+}
+
 console.log(boardPrint);
