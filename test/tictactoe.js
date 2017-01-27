f7506fadd52312125908625db7de8fdb3e4f0972
Chad Griffis <cmg123@me.com>
2017-01-27 15:50:41 -0600

    adds basic testing setup and structure

---
+++ test/tictactoe.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const ticTacToe = require('../tictactoe');
+
+const ticTacToeGame = {};
+
+describe('ticTacToe', function () {
+  it('should exist', function () {
+    expect(ticTacToeGame).to.exist;
+  })
+})
